e8216a234a7aecc521d265c7a158898a328bf505
tiennguyenvn2k <hiiamgamer2000@gmail.com>
2023-12-07 19:40:09 +0700

    hoan thanh bai tap

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace dotnet_Ung_dung_chuyen_doi_tien_te;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        const int RATE = 23000;
+        int tienUSD;
+        Console.WriteLine("Moi nhap so tien (USD) : ");
+        tienUSD = int.Parse(Console.ReadLine());
+        Console.WriteLine("So tien " + tienUSD + " USD" +" quy doi ra VND la: " 
+        + tienUSD*RATE +" VND");
+    }
+}
